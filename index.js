f3ab88a1b6fa7f37e0f34952514a5c0602823e2f
Moriba Sahr Kondeh <152665217+Kondeh1@users.noreply.github.com>
2025-12-29 15:58:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import session from "express-session";
 import passport from "passport";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const saltRounds = 20;
 env.config();
 
